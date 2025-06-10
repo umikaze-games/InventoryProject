232b9d47057b25f884a687505c6effd1c44acdd4
umikaze-games <c463384962@gmail.com>
2025-06-10 13:39:19 +0900

    init

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/Inv_PlayerController.h"
+#include "Inventory.h"
+
+void AInv_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(logInventory, Log, TEXT("Player Controller Begin play"));
+}
