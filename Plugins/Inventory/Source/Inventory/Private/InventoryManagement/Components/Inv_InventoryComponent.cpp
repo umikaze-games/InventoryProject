566af1d68f4855ce1f5f90d6bd7005d462289f08
umikaze-games <c463384962@gmail.com>
2025-06-17 22:26:26 +0900

    commit

---
+++ Plugins/Inventory/Source/Inventory/Private/InventoryManagement/Components/Inv_InventoryComponent.cpp
@@ -0,0 +1,28 @@
+﻿
+#include "InventoryManagement/Components/Inv_InventoryComponent.h"
+
+#include "Widgets/Invetory/InvetoryBase/Inv_InventoryBase.h"
+
+UInv_InventoryComponent::UInv_InventoryComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UInv_InventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	ConstructInventory();
+}
+
+void UInv_InventoryComponent::ConstructInventory()
+{
+	OwningController=Cast<APlayerController>(GetOwner());
+	OwningController = Cast<APlayerController>(GetOwner());
+	checkf(OwningController.IsValid(), TEXT("Inventory Component should have a Player Controller as Owner."))
+	if (!OwningController->IsLocalController()) return;
+
+	InventoryMenu = CreateWidget<UInv_InventoryBase>(OwningController.Get(), InventoryMenuClass);
+	InventoryMenu->AddToViewport();
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Invetory/Spatial/Inv_SPatialInventory.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Invetory/InvetoryBase/Inv_InventoryBase.h"
+#include "Inv_SPatialInventory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UInv_SPatialInventory : public UInv_InventoryBase
+{
+	GENERATED_BODY()
+};
