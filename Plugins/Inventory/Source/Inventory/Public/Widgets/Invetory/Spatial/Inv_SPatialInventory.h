f6ed8b86dfed2814fdfaf5b5e57138849bb9aae3
umikaze-games <c463384962@gmail.com>
2025-06-19 23:46:09 +0900

    commit

--- Plugins/Inventory/Source/Inventory/Public/Widgets/Invetory/Spatial/Inv_SPatialInventory.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Invetory/Spatial/Inv_SPatialInventory.h
@@ -6,11 +6,23 @@
 #include "Widgets/Invetory/InvetoryBase/Inv_InventoryBase.h"
 #include "Inv_SPatialInventory.generated.h"
 
-/**
- * 
- */
+class UInv_InventoryGrid;
+class UWidgetSwitcher;
+
 UCLASS()
 class INVENTORY_API UInv_SPatialInventory : public UInv_InventoryBase
 {
 	GENERATED_BODY()
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UWidgetSwitcher> Switcher;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UInv_InventoryGrid> Grid_Equippables;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UInv_InventoryGrid> Grid_Consumables;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UInv_InventoryGrid> Grid_Craftables;
 };
