30b926742959a6dfcfc6b23b24a6b0ce8f2ef45f
umikaze-games <c463384962@gmail.com>
2025-08-15 23:19:23 +0900

    commit

--- Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
@@ -37,20 +37,22 @@ FInv_SlotAvailabilityResult UInv_InventoryGrid::HasRoomForItem(const UInv_Invent
 FInv_SlotAvailabilityResult UInv_InventoryGrid::HasRoomForItem(const FInv_ItemManifest& Manifest)
 {
 	FInv_SlotAvailabilityResult Result;
-	Result.TotalRoomToFill = 7;
-	Result.bStackable = true;
-
-	FInv_SlotAvailability SlotAvailability;
-	SlotAvailability.AmountToFill = 2;
-	SlotAvailability.Index = 0;
-	
-	Result.SlotAvailabilities.Add(MoveTemp(SlotAvailability));
-	
-	FInv_SlotAvailability SlotAvailability2;
-	SlotAvailability2.AmountToFill = 5;
-	SlotAvailability2.Index = 1;
-	Result.SlotAvailabilities.Add(MoveTemp(SlotAvailability2));
-	
+	// Determine if the item is stackable.
+	// Determine how many stacks to add.
+	// For each Grid Slot:
+	//     If we don't have anymore to fill, break out of the loop early.
+	//     Is this index claimed yet?
+	//     Can the item fit here? (i.e. is it out of grid bounds?)
+	//     Is there room at this index? (i.e. are there other items in the way?)
+	//     Check any other important conditions - ForEach2D over a 2D range
+	//         Index claimed?
+	//         Has valid item?
+	//         Is this item the same type as the item we're trying to add?
+	//         If so, is this a stackable item?
+	//         If stackable, is this slot at the max stack size already?
+	//     How much to fill?
+	//     Update the amount left to fill
+	// How much is the Remainder?
 	return Result;
 }
 
